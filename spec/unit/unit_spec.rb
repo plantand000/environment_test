721f9f65274670f431763b77ecf6fe7a0ffc4166
plantand000 <57603429+plantand000@users.noreply.github.com>
2022-02-03 22:02:18 -0600

    finalized book collection

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', author: 'JK Rowling', price: '1.99', published_date: '2017-05-07')
   end
 
   it 'is valid with valid attributes' do
@@ -12,6 +12,9 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
+    subject.author = nil
+    subject.price = nil
+    subject.published_date = nil
     expect(subject).not_to be_valid
   end
 end
